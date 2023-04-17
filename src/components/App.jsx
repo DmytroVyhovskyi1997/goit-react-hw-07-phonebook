e92bc22d17117cdc8ee4d106579c7797a246a9e4
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-04-17 21:43:28 +0300

    push

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
+export default App;
